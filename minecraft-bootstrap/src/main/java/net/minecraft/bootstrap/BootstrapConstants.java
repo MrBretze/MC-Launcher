307404ca242d935cc2cfcdb6054abf0167b05703
MrBretzel <Axelolo68@gmail.com>
2015-04-30 01:24:41 +0200

    Initial commit
    1er

---
+++ minecraft-bootstrap/src/main/java/net/minecraft/bootstrap/BootstrapConstants.java
@@ -0,0 +1,8 @@
+package net.minecraft.bootstrap;
+
+public class BootstrapConstants {
+    public static final String MD5_FILE = "http://monsite.com/launcher/md5.txt"; //Lien vers le fichier md5.txt situé dans votre dossier launcher sur votre serveur FTP.
+    public static final String SERVER_NAME = "MON_NOM_DE_SERVEUR"; //Votre nom de serveur (IDENTIQUE A CELUI DANS LE LAUNCHER)
+    public static final String APPLICATION_NAME = "minecraftapp"; //nom donné à votre dossier d'installation situé dans %appdata% sur votre PC.
+    public static final String LAUNCHER_URL = "http://monsite.com/launcher/launcher.jar"; //Lien vers votre ficher launcher.jar situé dans votre serveur FTP.
+}
